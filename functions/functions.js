ee7edd899f797c5763c604fb481b1a5cb6362113
Tom Garet <tom.garet.fr@gmail.com>
2022-08-20 01:13:39 +0200

    Update functions.js
    correction v2

--- functions/functions.js
+++ functions/functions.js
@@ -1,5 +1,5 @@
 const Item = require("./assets/Classes/Item");
-const Item = require("./assets/Classes/JobsItems");
+const JobsItems = require("./assets/Classes/JobsItems");
 
 async function get_frame_fr(){
     let tmp_frame;
